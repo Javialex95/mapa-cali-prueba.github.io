015b435e4e00b2664af94c06ae98858a9acef7e1
Javier López <alexjls95@gmail.com>
2020-03-03 14:49:56 -0500

    Actualizado con Google Maps

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 'use strict'
 // MAPA
-var mymap = L.map('mapid').setView([3.4372201, -76.5224991], 13);
-const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png '
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('mapid'), {
+    center: {lat: 3.4372201, lng: -76.5224991},
+    zoom: 12.5,
+    mapTypeId: 'hybrid'
+  });
 
-L.tileLayer(tilesProvider, {
-    maxZoom: 18,
-}).addTo(mymap);
+  var marker = new google.maps.Marker({
+      position: {lat: 3.4372201, lng: -76.5224950},
+      map: map
+  })
+
+  var infoWindow = new google.maps.InfoWindow({
+      content: '<h1>Esta es una ventana de prueba que no creo que nos sirva jaja</h1>'
+  })
+
+  var marker2 = new google.maps.Marker({
+    position: {lat: 3.457699, lng: -76.50089},
+    map: map,
+    icon: 'https://img.icons8.com/plasticine/100/000000/filter.png'
+})
+
+
+marker2.addListener('click', function(){
+    infoWindow.open(map, marker2)
+});
+
+}
 
 // Abrir seccion
 function menuOpen(){
